c0ffd4976bd24491e69d64ddb9c6dc6483d60b99
ronakjpatel <40088060+ronakjpatel@users.noreply.github.com>
2023-10-27 23:05:53 -0400

    .

--- Part 1/convert_greyscale_and_resize.py
+++ Part 1/convert_greyscale_and_resize.py
@@ -1,30 +1,29 @@
 from PIL import Image
 import os
 
-# Define the source folder
+# the source folder
 source_dir = "AppliedAI/Data"
 
-# Set the desired size of image in pixels and DPI
+# desired size of image
 new_image_size = (48, 48)
 dpi = (96, 96)
 
-# Use os.walk to traverse all subdirectories and their contents
+# traversing all subdirectories
 for root, dirs, files in os.walk(source_dir):
     for img_file in files:
-        # Check if the file is an image and image extensions can be added when needed.
+        
         if img_file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
-            # Construct the full path to the image
+            # Constructing the full path
             image_path = os.path.join(root, img_file)
-            
-            # Open the image
+        
             with Image.open(image_path) as img:
-                # Resize the image to the desired size
+                # Resizing
                 resized_img = img.resize(new_image_size)
                 
-                # Convert the image to grayscale
+                # Converting to Grayscale
                 grayscale_img = resized_img.convert("L")
 
-                # Save the grayscale image back to the same path, overwriting the original
+                #saving
                 grayscale_img.save(image_path, dpi=dpi)
 
 print("Successfully resized and converted images to grayscale in-place.")
--- Part 1/filtering_locating_faces.py
+++ Part 1/filtering_locating_faces.py
@@ -1,13 +1,13 @@
 import cv2
 import os
 
-# Path to the directory containing the images
+# source path
 source_dir = 'AppliedAI/Data'
 
-# Setting path to the XML cascade classifier file for detecting face
+# XML cascade classifier file for detecting face
 cascade_classifier_path = "AppliedAI/Part 1/haarcascade_frontalface_alt2.xml"
 
-# Load the cascade classifier for face detection
+# Loading cascade file
 face_cascade_classifier = cv2.CascadeClassifier(cascade_classifier_path)
 
 # Function creation to detect and save faces in an image
@@ -17,7 +17,7 @@ def detect_and_save_faces_in_place(image_path):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         detected_faces = face_cascade_classifier.detectMultiScale(gray_image, 1.1, 4)
         
-        # If faces are detected, process the image
+        # If faces are detected
         if len(detected_faces) > 0:
             # Loop through detected faces and save them
             for i, (x, y, w, h) in enumerate(detected_faces):
@@ -26,15 +26,15 @@ def detect_and_save_faces_in_place(image_path):
                 face_path = os.path.join(os.path.dirname(image_path), face_filename)
                 cv2.imwrite(face_path, face)
 
-            # Removal of the original image after processing it successfully
+            # Removal
             os.remove(image_path)
         else:
-            # If no faces are detected, simply delete the photo
+            # If no faces, delete the photo
             os.remove(image_path)
     except Exception as e:
         print(f"Error processing {image_path}: {str(e)}")
 
-# Traverse through subdirectories and process images
+
 for root, dirs, files in os.walk(source_dir):
     for file in files:
         if file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
