c9eaa3b91faa0dad5575e2294ec544f0b5167d0f
vijendraahirwal <vijendraahirwal@gmail.com>
2023-12-01 20:18:15 -0500

    Changing relative path for part-1 complete

--- Part 1/image_labeling.py
+++ Part 1/image_labeling.py
@@ -1,7 +1,7 @@
 import os
 
 #source directory
-source_dir = "AppliedAI/Data"
+source_dir = r"Data"
 
 
 def rename_files_in_directory(directory_path):
@@ -15,6 +15,7 @@ def rename_files_in_directory(directory_path):
         file_name, file_extension = os.path.splitext(file)
         original_file_path = os.path.join(directory_path, file)
         new_file_path = os.path.join(directory_path, f"{new_name}{file_extension}")
+        #print(f"Renaming {original_file_path} to {new_file_path}")
         os.rename(original_file_path, new_file_path)
         sequence_number += 1
 
@@ -23,6 +24,7 @@ for root, dirs, files in os.walk(source_dir):
     for directory in dirs:
         #Building full path to the subdirectory
         directory_path = os.path.join(root, directory)
+        #print(directory_path)
         rename_files_in_directory(directory_path)
 
 print("File renaming completed.")
